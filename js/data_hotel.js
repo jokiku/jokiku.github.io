6f4efbfa880ab1aec877b13c24323aef011eb993
ali5alkaf5 <ali5alkaf5@gmail.com>
2020-05-13 23:47:40 +0700

    add details to barcodeaa

---
+++ js/data_hotel.js
@@ -0,0 +1,44 @@
+data = {
+    "Hotel": [
+        {
+            "Nama": "Kempong Tourist",
+            "Kota": "Malang",
+            "Provinsi": "Jawa Timur",
+            "Negara": "Indonesia",
+            "Harga": "159084",
+            "image": "kompung.jpg"
+        },
+        {
+            "Nama": "Hotel Malang",
+            "Kota": "Malang",
+            "Provinsi": "Jawa Timur",
+            "Negara": "Indonesia",
+            "Harga": "90545",
+            "image": "malang.png"
+        },
+        {
+            "Nama": "Hotel Sentosa",
+            "Kota": "Malang",
+            "Provinsi": "Jawa Timur",
+            "Negara": "Indonesia",
+            "Harga": "172213",
+            "image": "grand.jpg"
+        },
+        {
+            "Nama": "Life Hotels Surabaya",
+            "Kota": "Surabaya",
+            "Provinsi": "Jawa Timur",
+            "Negara": "Indonesia",
+            "Harga": "119871",
+            "image": "life.png"
+        },
+        {
+            "Nama": "Swiss-Belinn",
+            "Kota": "Surabaya",
+            "Provinsi": "Jawa Timur",
+            "Negara": "Indonesia",
+            "Harga": "372800",
+            "image": "swiss.png"
+        }
+    ]
+};
